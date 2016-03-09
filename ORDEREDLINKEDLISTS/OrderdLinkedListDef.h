37c1bb5ece2b0b0d42c4b333e6c01becf4a565a4
XealRetro <megalodonxgaming@gmail.com>
2016-03-09 10:32:54 -0500

    Formatting change

--- ORDEREDLINKEDLISTS/OrderdLinkedListDef.h
+++ ORDEREDLINKEDLISTS/OrderdLinkedListDef.h
@@ -11,21 +11,25 @@ public:
 	//Function to determine whether searchItem is in the list.
 	//Postcondition: Returns true if searchItem is in the list,
 	// otherwise the value false is returned.
+
 	void insert(const Type& newItem);
 	//Function to insert newItem in the list.
 	//Postcondition: first points to the new list, newItem
 	// is inserted at the proper place in the list, and
 	// count is incremented by 1.
+
 	void insertFirst(const Type& newItem);
 	//Function to insert newItem at the beginning of the list.
 	//Postcondition: first points to the new list, newItem is
 	// inserted at the beginning of the list, last points to the
 	// last node in the list, and count is incremented by 1.
+
 	void insertLast(const Type& newItem);
 	//Function to insert newItem at the end of the list.
 	//Postcondition: first points to the new list, newItem is
 	// inserted at the end of the list, last points to the
 	// last node in the list, and count is incremented by 1.
+
 	void deleteNode(const Type& deleteItem);
 	//Function to delete deleteItem from the list.
 	//Postcondition: If found, the node containing deleteItem is
