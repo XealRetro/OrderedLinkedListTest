f41cc23fac60eeb3c985e1fe8d765fa81dfd4119
XealRetro <megalodonxgaming@gmail.com>
2016-03-09 18:48:36 -0500

    More formatting changes

--- ORDEREDLINKEDLISTS/OrderedLinkedListImp.cpp
+++ ORDEREDLINKEDLISTS/OrderedLinkedListImp.cpp
@@ -49,13 +49,19 @@ void orderedLinkedList<Type>::insert(const Type& newItem)
 		current = first;
 		found = false;
 		while (current != NULL && !found) //search the list
+		{
 			if (current->info >= newItem)
+			{
 				found = true;
+			}
+				
 			else
 			{
 				trailCurrent = current;
 				current = current->link;
 			}
+		}
+			
 		if (current == first) //Case 2
 		{
 			newNode->link = first;
@@ -67,7 +73,10 @@ void orderedLinkedList<Type>::insert(const Type& newItem)
 			trailCurrent->link = newNode;
 			newNode->link = current;
 			if (current == NULL)
+			{
 				last = newNode;
+			}
+				
 			count++;
 		}
 	}//end else
@@ -84,5 +93,73 @@ void orderedLinkedList<Type>::insertLast(const Type& newItem)
 {
 	insert(newItem);
 }//end insertLast
+
+template <class Type>
+void orderedLinkedList<Type>::deleteNode(const Type& deleteItem)
+{
+	nodeType<Type> *current; //pointer to traverse the list
+	nodeType<Type> *trailCurrent; //pointer just before current
+	bool found;
+	if (first == NULL) //Case 1
+	{
+		cout << "Cannot delete from an empty list." << endl;
+	}
+		
+	else
+	{
+		current = first;
+		found = false;
+		while (current != NULL && !found) //search the list
+		{
+			if (current->info >= deleteItem)
+			{
+				found = true;
+			}
+				
+			else
+			{
+				trailCurrent = current;
+				current = current->link;
+			}
+		}
+			
+		if (current == NULL) //Case 4
+		{
+			cout << "The item to be deleted is not in the list." << endl;
+		}
+			
+		else
+		{
+			if (current->info == deleteItem) //the item to deleted is in the list
+			{
+				{
+				if (first == current) //Case 2
+				{
+					first = first->link;
+					if (first == NULL)
+						last = NULL;
+					delete current;
+				}
+				else //Case 3
+				{
+					trailCurrent->link = current->link;
+					if (current == last)
+						last = trailCurrent;
+					delete current;
+				}
+				count--;
+			}
+			}
+			
+			else //Case 4
+			{
+				cout << "The item to be deleted is not in the list." << endl;
+			}
+		}
+			
+				
+	}
+}//end deleteNode
+			
 
 	
